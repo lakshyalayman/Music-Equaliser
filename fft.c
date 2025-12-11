77f5c6a6651a3f8826a75bf9a7c473a7eb4cde58
lakshyalayman <lakshya1.web@gmail.com>
2025-12-11 10:58:14 +0530

    Concept

--- fft.c
+++ fft.c
@@ -20,16 +20,34 @@ int main(){
     float t = (float)i/n;
     in[i] = sinf(2*pi*t) + cosf(2*pi*t*3);
   }
-
-  for(size_t f = 0;f<n;++f){
+  
+  for(size_t f = 0;f<n/2;++f){
     out[f] = 0;
-    for(size_t i = 0;i<n;++i){
+    out[f + n/2] = 0;
+
+    for(size_t i = 0;i<n;i+=2){
       float t = (float)i/n;
-      out[f] += in[i]*cexp(I*2*pi*f*t);
-      // out[f] += in[i]*sinf(2*pi*f*t);
+      float complex cmpx = in[i]*cexp(2*I*pi*t*f);
+      out[f] += cmpx;
+      out[f+n/2] += cmpx;
     }
-    
+    for(size_t i = 1;i<n;i+=2){
+      float t = (float)i/n;
+      float complex cmpx = in[i]*cexp(2*I*pi*t*f);
+      out[f] += cmpx;
+      out[f+n/2] -= cmpx;
+    }
   }
+
+  // for(size_t f = 0;f<n;++f){
+  //   out[f] = 0;
+  //   for(size_t i = 0;i<n;++i){
+  //     float t = (float)i/n;
+  //     out[f] += in[i]*cexp(I*2*pi*f*t);
+  //     // out[f] += in[i]*sinf(2*pi*f*t);
+  //   }
+  //   
+  // }
   for(size_t f = 0;f<n;++f){
     printf("%02zu: real(%.2f) complex(%.2fi)\n",f,creal(out[f]),cimag(out[f]));
   }
